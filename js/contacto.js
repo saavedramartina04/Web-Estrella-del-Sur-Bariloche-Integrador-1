d1c11f9507dcfe8a98ef761f044d71879af480c7
saavedramartina04 <saavedramartina04@gmail.com>
2026-05-14 20:23:40 -0300

    Proyecto finalizado

--- js/contacto.js
+++ js/contacto.js
@@ -2,9 +2,50 @@
 //variables globales
 //--------------------------------------------------
 
+const formulario = document.querySelector('form')
+
+formulario.addEventListener('submit', validarFormulario)
+
+function validarFormulario(e){
+
+    e.preventDefault()
+
+    const refNombre = document.querySelector('#nombre')
+    const refEmail = document.querySelector('#email')
+    const refComentarios = document.querySelector('#comentarios')
+
+    const nombre = refNombre.value
+    const email = refEmail.value
+    const comentarios = refComentarios.value
 
 
+    if(nombre == ''){
+        alert('Debe ingresar un nombre')
+        return
+    }
 
+    if(email == ''){
+        alert('Debe ingresar un email')
+        return
+    }
+
+    if(comentarios == ''){
+        alert('Debe escribir un comentario')
+        return
+    }
+
+    if(!email.includes('@')){
+        alert('Email inválido')
+        return
+    }
+
+
+    alert('Formulario enviado correctamente')
+
+    refNombre.value = ''
+    refEmail.value = ''
+    refComentarios.value = ''
+}
 //--------------------------------------------------
 //funciones globales
 //--------------------------------------------------
--- js/inicio.js
+++ js/inicio.js
@@ -12,28 +12,35 @@ function representarCardsProductos() {
     if (productos.length) {
         for (let i = 0; i < productos.length; i++) {
 
-            cards += `
-            <section class="card">
-                <h3>${productos[i].nombre}</h3>
-                <img src="${productos[i].foto}" alt="">
-                <div class="info">
-                    <p><b>Precio:</b> $${productos[i].precio}</p>
-                    <p><b>Stock:</b> ${productos[i].stock}</p>
-                    <p><b>Marca:</b> ${productos[i].marca}</p>
-                    <p><b>Categoria:</b> ${productos[i].categoria}</p>
-                    <p><b>Descripción corta:</b> ${productos[i].descripcionCorta}</p>
-                    <p><b>Descripción larga:</b> ${productos[i].descripcionLarga}</p>
-                    <p><b>Edad desde:</b> ${productos[i].edadDesde}</p>
-                    <p><b>Edad hasta:</b> ${productos[i].edadHasta}</p>
-                    <br>
-                    <p style="color:gold">
-                        <b>Envio bonificado:</b>
-                        ${productos[i].envio ? 'Si' : 'No'}
-                    </p>
-                </div>
-            </section>
-           `
-        }
+           cards += `
+
+        <section class="card">
+            <h3>${productos[i].nombre}</h3>
+            <img src="${productos[i].foto}" alt="">
+            <div class="info-visible">
+                <p class="precio">
+                    $${productos[i].precio}
+                </p>
+                <p class="descripcion">
+                    ${productos[i].descripcionCorta}
+                </p>
+                <a href="#" class="btn-carrito">
+                    Agregar al carrito
+                </a>
+            </div>
+            <div class="info">
+                <p><b>Stock:</b> ${productos[i].stock}</p>
+                <p><b>Marca:</b> ${productos[i].marca}</p>
+                <p><b>Categoria:</b> ${productos[i].categoria}</p>
+                <p><b>Descripción larga:</b>${productos[i].descripcionLarga}</p>
+                <p><b>Edad desde:</b>${productos[i].edadDesde}</p>
+                <p><b>Edad hasta:</b>${productos[i].edadHasta}</p>
+            </div>
+
+        </section>
+
+        `
+    }
     } else {
         cards += '<h2>No se encontraron productos para mostrar</h2>'
     }
